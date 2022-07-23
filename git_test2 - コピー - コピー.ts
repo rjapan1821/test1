2a8fea315c9548a1e17e5af33f68712e14085ce1
rjapan1821 <60636084+rjapan1821@users.noreply.github.com>
2022-07-23 10:09:18 +0900

    [Add] index

---
+++ git_test2 - コピー - コピー.ts
@@ -0,0 +1,14 @@
+﻿
+//―――――――――OBJ_Git――――――――――-----―――
+
+const Git = new Entity()
+Git.addComponent(new GLTFShape("models/6.Youtube_Studio_gltf.gltf"))   
+Git.addComponent(new Transform({
+    position: new Vector3(8.08, -0.066, 7.932),
+    rotation: new Quaternion(0, 0, 0, 1),
+    scale: new Vector3(1, 1, 1) 
+}))
+
+engine.addEntity(Git)
+
+
